fb4afa8459f32764dc9c142b65e3707883366d4b
Yadav, Sunil <sunil016@yahoo.com>
2023-04-10 12:37:16 +0530

    #3 - add more vars and comments

--- src/3-Using-Variables-Types-And-Enums/index.ts
+++ src/3-Using-Variables-Types-And-Enums/index.ts
@@ -1,20 +1,24 @@
 // string, number, boolean, array, undefined, null, any
 
-let firstName: string | null;
+let firstName: string | null; // Union Type
 firstName = 'Dan';
 
-let age: number;
+let age: number; // Number
 age = 45;
 
-let hasPurchased = true;
+let hasPurchased = true; // Boolean
 
-let productNames: string[] = [];
+let productNames: string[] = []; // String Array
 productNames.push('Basketball');
 
-let petCount: number[] = [];
+let petCount: number[] = []; // Number Array
 petCount.push(5);
 
-console.log(firstName, age, hasPurchased, productNames, petCount);
+let anyParam: any[] = []; // Any Array
+anyParam.push(5);
+anyParam.push("Sunil");
+
+console.log(firstName, age, hasPurchased, productNames, petCount, anyParam);
 
 let productType = 'sports'; // homeGoods, grocery ("magic string")
 if (productType === 'sports') {
@@ -22,12 +26,21 @@ if (productType === 'sports') {
 }
 
 // Using Enums
-enum ProductType {
-  Sports,
-  HomeGoods,
-  Groceries,
+enum ProductType { // const enum, if you only care about comparision not values since it uses index of enums
+  Sports,  // Sports="sports",
+  HomeGoods, // HomeGoods="homeGoods",
+  Groceries // Groceries="groceries",
 }
 let pt = ProductType.Sports;
 if (pt === ProductType.Sports) {
+  console.log('Its Value inside :: ',ProductType.Sports);
   console.log('Found sports product type.');
 }
+
+// Type Interferance
+let fName = "Sunil"; // Based on Value
+
+let dfName: string;
+dfName="Sunil" 
+
+let productNamesArray = [] // Default is any[], not recommended.
